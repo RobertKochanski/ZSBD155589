971012636891d9dc7b7ac88218cc78fc06f3d463
RobertKochanski <kochanskirobert1999@gmail.com>
2025-10-24 12:07:50 +0200

    fix views naming

--- cwiczenia/lab3/lab3.sql
+++ cwiczenia/lab3/lab3.sql
@@ -15,41 +15,41 @@ create or replace view v_wysokie_pensje as
 drop view v_wysokie_pensje;
 
 // 4.
-create view finance_employees as
+create view v_finance_employees as
     select e.employee_id, e.last_name, e.first_name
     from employees e
         join departments d on d.department_id = e.department_id
     where d.department_name = 'Finance';
     
 // 5.
-create view salary_5000_12000 as
+create view v_salary_5000_12000 as
     select employee_id, last_name, first_name, salary, job_id, email, hire_date
     from employees
     where salary between 5000 and 12000;
     
 // 6. zmiany na widokach powoduja zmiany na tabelach
 // a.
-// do finance_employees nie można dodać rekordu ponieważ nie ma pokrycia w wymaganych kolumnach
+// do v_finance_employees nie można dodać rekordu ponieważ nie ma pokrycia w wymaganych kolumnach
 // tabel na bazie których powstał widok
-insert into finance_employees values (207, 'Andrzej', 'Kowalski');
+insert into v_finance_employees values (207, 'Andrzej', 'Kowalski');
 
-// w salary_5000_12000 można dodać rekord ponieważ posiada w sobie wymagane kolumny
+// w v_salary_5000_12000 można dodać rekord ponieważ posiada w sobie wymagane kolumny
 // należy jednak uważać na kolejność kolumn która różni się od tabeli bazowej
-insert into salary_5000_12000
+insert into v_salary_5000_12000
 values (207, 'Andrzej', 'Kowalski', 9999, 'FI_ACCOUNT', 'AKowalski', DATE '2020-07-01');
 
 // b.
-update salary_5000_12000
+update v_salary_5000_12000
 set last_name = 'Kowalski',
     first_name = 'Andrzej'
 where employee_id = 207;
 
 // c.
-delete from salary_5000_12000
+delete from v_salary_5000_12000
 where employee_id = 207;
 
 // 7.
-create view atleast_4_employees as
+create view v_atleast_4_employees as
     select d.department_id, d.department_name, count(e.employee_id) as emp_number, round(avg(e.salary)) as avg_salary, max(e.salary) as max_salary
     from departments d
         join employees e on d.department_id = e.department_id
@@ -58,23 +58,23 @@ create view atleast_4_employees as
     
 // a.
 // "nie można modyfikować kolumny, która odwzorowuje się do tabeli nie zachowującej kluczy"
-insert into atleast_4_employees
+insert into v_atleast_4_employees
 values (100, 'Finance', 123, 9999, 12121);
 
 // 8.
-create view salary_5000_12000_with_check as
+create view v_salary_5000_12000_with_check as
     select employee_id, last_name, first_name, salary, job_id, email, hire_date
     from employees
     where salary between 5000 and 12000
 with check option;
 
 // a1.
-insert into salary_5000_12000_with_check
+insert into v_salary_5000_12000_with_check
 values (209, 'Andrzej', 'Kowalski', 9999, 'FI_ACCOUNT', 'AKowalski', DATE '2020-07-01');
 
 // a2.
 // "naruszenie klauzuli WHERE dla perspektywy z WITH CHECK OPTION"
-insert into salary_5000_12000_with_check
+insert into v_salary_5000_12000_with_check
 values (208, 'Andrzej', 'Kowalski', 20000, 'FI_ACCOUNT', 'AKowalski', DATE '2020-07-01');
 
 // 9.
